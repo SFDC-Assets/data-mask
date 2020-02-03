4aeef2e6e47a6783eb066b44ad0f17be02c72202
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-03 16:52:32 -0600

    shape, psl, psa

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install -w 20 -r -p 04t3k0000027AGeAAM
 sfdx force:source:push
+sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
+sfdx shane:user:permset:assign -l User -g User -n datamask
 sfdx force:org:open
