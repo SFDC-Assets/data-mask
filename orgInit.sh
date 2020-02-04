423d049edd11d2169f9baffd984a8adefac0a603
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-04 07:53:05 -0600

    v47 scripted

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:package:install -w 20 -r -p 04t3k0000027AGeAAM
-sfdx force:source:push
 sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
 sfdx shane:user:permset:assign -l User -g User -n datamask
-sfdx force:org:open
+sfdx force:source:push
+sfdx force:org:open -p /lightning/n/datamask__Data_Mask
