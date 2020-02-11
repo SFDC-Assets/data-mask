b6ef84d558221bbe9c86d3c50fd4e8548ceb7b8a
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-11 10:48:46 -0600

    opens to contacts list view

--- orgInit.sh
+++ orgInit.sh
@@ -3,4 +3,4 @@ sfdx force:package:install -w 20 -r -p 04t3k0000027AGeAAM
 sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
 sfdx shane:user:permset:assign -l User -g User -n datamask
 sfdx force:source:push
-sfdx force:org:open -p /lightning/n/datamask__Data_Mask
+sfdx force:org:open -p /lightning/o/Contact/list?filterName=AllContacts
