9475cabd8645c0f1c9201a456a47dc01075a8190
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-02 09:07:06 -0500

    bump package version

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:package:install -w 20 -r -p 04t3k0000027AGeAAM
+sfdx force:package:install -w 20 -r -p 04t3k000001yuer
 sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
 sfdx shane:user:permset:assign -l User -g User -n datamask
 sfdx force:source:push
