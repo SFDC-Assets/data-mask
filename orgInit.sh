08a7906b6024583664fbc6346711efa9e72f8253
chadevanssf <chad.evans@salesforce.com>
2021-07-06 12:27:43 -0400

    Document how to update the package for DM

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+# Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
 sfdx force:package:install -w 20 -r -p 04t3k0000027CCn
 sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
 sfdx shane:user:permset:assign -l User -g User -n datamask
