c1c616cb3fe6058aba6593e1ac7befe041ddb63f
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-21 22:05:28 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@ sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p dat
 
 # Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
 sf package install -w 20 -r -p 04t3k0000023DoZ
-sf org assign permset -n datamask_DataMaskUserPsl
+sf shane user psl -n datamask_DataMaskUserPsl -g User -l User
 sf org assign permset -n datamask
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
