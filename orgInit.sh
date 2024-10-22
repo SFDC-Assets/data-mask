9781755902b745fc2a3a67465932a5e24f7487fe
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-21 22:01:03 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,9 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix data -o mask.demo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e mask.demo
+
 # Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
-sfdx force:package:install -w 20 -r -p 04t3k0000023DoZ
-sfdx shane:user:psl -l User -g User -n datamask_DataMaskUserPsl
-sfdx shane:user:permset:assign -l User -g User -n datamask
-sfdx force:source:push
-sfdx shane:user:password:set -g User -l User -p salesforce1
-sfdx force:org:open -p /lightning/o/Contact/list
+sf package install -w 20 -r -p 04t3k0000023DoZ
+sf org assign permset -n datamask_DataMaskUserPsl
+sf org assign permset -n datamask
+sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open -p /lightning/o/Contact/list
