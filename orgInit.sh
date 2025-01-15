3c25e6ce4f06f6b6cd5dd94f02dbde66cfd0de1a
zhou059 <zhou059@gmail.com>
2025-01-15 15:50:56 -0800

    latest package

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e mask.demo
 
 # Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
-sf package install -w 20 -r -p 04t3k0000023DoZ
+sf package install -w 20 -r -p 04tKV000002AghV
 sf shane user psl -n datamask_DataMaskUserPsl -g User -l User
 sf org assign permset -n datamask
 sf project deploy start
