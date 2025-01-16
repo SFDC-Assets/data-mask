c01d70d08d303f8dc84c3ac3a292c09cd5ba73a9
zhou059 <zhou059@gmail.com>
2025-01-15 16:08:11 -0800

    create custom library

--- orgInit.sh
+++ orgInit.sh
@@ -4,6 +4,8 @@ sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p dat
 sf package install -w 20 -r -p 04tKV000002AghV
 sf shane user psl -n datamask_DataMaskUserPsl -g User -l User
 sf org assign permset -n datamask
+sf apex run -f scripts/apex/createCustomLibrary.apex
+
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
 sf org open -p /lightning/o/Contact/list
