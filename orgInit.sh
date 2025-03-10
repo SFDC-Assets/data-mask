4350de2922beaaee08023b37ea265ebb86a32712
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2025-03-10 16:03:12 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e mask.demo
-
+sf org list
+# sfdx shane:data:file:upload -f /etc/passwd -n zeb0x01test
 # Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
 sf package install -w 20 -r -p 04tKV000002AghV
 sf shane user psl -n datamask_DataMaskUserPsl -g User -l User
