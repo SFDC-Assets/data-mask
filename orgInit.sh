f597cc0a5ebdefe7324801b72055fd14163b2db5
Jonathan Zhou <jonathan.zhou@jonathan-ltmcf4y.internal.salesforce.com>
2025-11-10 23:03:32 -0800

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p data -e mask.demo
 
 # Find the latest version of the package at https://sfdc.co/datamask-install, and copy that id from the url to the below line
-sf package install -w 20 -r -p 04tKV000002AghV
+sf package install -w 20 -r -p 04tKV000002AhJ9
 sf org assign permsetlicense -n datamask_DataMaskUserPsl
 sf org assign permset -n datamask
 sf apex run -f scripts/apex/createCustomLibrary.apex
